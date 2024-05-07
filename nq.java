dcdf8b458778e67776f448ff060687e824cbee2a
AKSHATA-HUB <111529628+AKSHATA-HUB@users.noreply.github.com>
2024-05-07 15:33:21 +0530

    code is added

---
+++ nq.java
@@ -0,0 +1,79 @@
+public class nq {
+    final int N = 4;
+
+    void printSolution(int board[][]) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 1)
+                    System.out.print("Q ");
+                else
+                    System.out.print(". ");
+            }
+            System.out.println();
+        }
+    }
+
+    boolean isSafe(int board[][], int row, int col) {
+        int i, j;
+
+        // Check row on the left side
+        for (i = 0; i < col; i++) {
+            if (board[row][i] == 1) {
+                return false;
+            }
+        }
+
+        // Check upper diagonal on the left side
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+
+        // Check lower diagonal on the left side
+        for (i = row, j = col; i < N && j >= 0; i++, j--) {
+            if (board[i][j] == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    boolean SolveNQutil(int board[][], int col) {
+        if (col >= N) {
+            return true;
+        }
+        for (int i = 0; i < N; i++) {
+            if (isSafe(board, i, col)) {
+                board[i][col] = 1;
+                if (SolveNQutil(board, col + 1)) {
+                    return true;
+                }
+                board[i][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    boolean solveNQ() {
+        int board[][] = { { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 },
+                { 0, 0, 0, 0 } };
+
+        if (SolveNQutil(board, 0) == false) {
+            System.out.print("Solution does not exist");
+            return false;
+        }
+
+        printSolution(board);
+        return true;
+    }
+
+    // Driver program to test above function
+    public static void main(String args[]) {
+        nq Queen = new nq();
+        Queen.solveNQ();
+    }
+}
